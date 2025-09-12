119cf007f06defaa89c29284e7443676c4a0a617
iScreenager <vaishnavimzp100@gmail.com>
2025-09-13 02:08:18 +0530

    fix: env fix

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = process.env.BACKEND_URL;
+const URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 export const socket = io(URL, {
   withCredentials: true,
   transports: ["websocket"],
